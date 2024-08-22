3d3c970eb9b148c8e5702b274b1f079319c95e6a
ferreira.jim <jimdouglasmmferreira@gmail.com>
2024-08-22 11:05:29 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import { ThemeProvider } from 'styled-components'
+
+import { Button } from './components/Button'
+import { GlobalStyle } from './styles/global'
+import { defaultTheme } from './styles/themes/default'
+
+export const App = () => {
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <Button />
+      <Button />
+      <Button />
+      <Button />
+
+      <GlobalStyle />
+    </ThemeProvider>
+  )
+}
